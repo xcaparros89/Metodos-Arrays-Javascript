27c2256c700860dcc3c41bca1fbf7bcf1186a13f
Xavier Caparros Gelabert <xcaparros@hotmail.com>
2020-10-01 16:49:06 +0200

    done

--- Ejercicio.js
+++ Ejercicio.js
@@ -3,7 +3,7 @@
 // Given an array of cities, return an array with the first letter of each city’s name capitalized. You can use ES5 or ES6 syntax, whichever you feel more comfortable with at this point.
 
 // array of cities:
-let cities = ["mimi", "barcelona", "madrid", "amsterdam", "berlin", "sao paulo", "lisbon", "mexico city", "paris"];
+let cities = ["miami", "barcelona", "madrid", "amsterdam", "berlin", "sao paulo", "lisbon", "mexico city", "paris"];
 
 
 //ES5 =>
@@ -11,9 +11,14 @@ let cities = ["mimi", "barcelona", "madrid", "amsterdam", "berlin", "sao paulo",
 
 
 
+
 //ES6 =>
 
-
+let CapitalizedCities = (array) =>{
+    array.map(word=>{
+      return word[0].toUpperCase() + word.splice(1)
+    })
+}
 
 
 
@@ -25,66 +30,66 @@ let cities = ["mimi", "barcelona", "madrid", "amsterdam", "berlin", "sao paulo",
 // So the whole class is represented as an array of objects (each object contains the data about that student), and we need to get a new array of objects, but this time the objects will contain only student´s name, and their final grade. Let´s do it.
 
 const students = [
-    {
-      name: "Tony Parker",
-      firstProject: 80,
-      secondProject: 75,
-      finalExam: 90
-    },
-    {
-      name: "Marc Barchini",
-      firstProject: 84,
-      secondProject: 65,
-      finalExam: 65
-    },
-    {
-      name: "Claudia Lopez",
-      firstProject: 45,
-      secondProject: 95,
-      finalExam: 99
-    },
-    {
-      name: "Alvaro Briattore",
-      firstProject: 82,
-      secondProject: 92,
-      finalExam: 70
-    },
-    {
-      name: "Isabel Ortega",
-      firstProject: 90,
-      secondProject: 32,
-      finalExam: 85
-    },
-    {
-      name: "Francisco Martinez",
-      firstProject: 90,
-      secondProject: 55,
-      finalExam: 78
-    },
-    {
-      name: "Jorge Carrillo",
-      firstProject: 83,
-      secondProject: 77,
-      finalExam: 90
-    },
-    {
-      name: "Miguel López",
-      firstProject: 80,
-      secondProject: 75,
-      finalExam: 75
-    },
-    {
-      name: "Carolina Perez",
-      firstProject: 85,
-      secondProject: 72,
-      finalExam: 67
-    },
-    {
-      name: "Ruben Pardo",
-      firstProject: 89,
-      secondProject: 72,
-      finalExam: 65
-    }
+  {
+    name: "Tony Parker",
+    firstProject: 80,
+    secondProject: 75,
+    finalExam: 90
+  },
+  {
+    name: "Marc Barchini",
+    firstProject: 84,
+    secondProject: 65,
+    finalExam: 65
+  },
+  {
+    name: "Claudia Lopez",
+    firstProject: 45,
+    secondProject: 95,
+    finalExam: 99
+  },
+  {
+    name: "Alvaro Briattore",
+    firstProject: 82,
+    secondProject: 92,
+    finalExam: 70
+  },
+  {
+    name: "Isabel Ortega",
+    firstProject: 90,
+    secondProject: 32,
+    finalExam: 85
+  },
+  {
+    name: "Francisco Martinez",
+    firstProject: 90,
+    secondProject: 55,
+    finalExam: 78
+  },
+  {
+    name: "Jorge Carrillo",
+    firstProject: 83,
+    secondProject: 77,
+    finalExam: 90
+  },
+  {
+    name: "Miguel López",
+    firstProject: 80,
+    secondProject: 75,
+    finalExam: 75
+  },
+  {
+    name: "Carolina Perez",
+    firstProject: 85,
+    secondProject: 72,
+    finalExam: 67
+  },
+  {
+    name: "Ruben Pardo",
+    firstProject: 89,
+    secondProject: 72,
+    finalExam: 65
+  }
 ]
 
 
@@ -95,9 +100,17 @@ const students = [
 
 //ES6 =>
 
+let ArrayFinalNote = (array) => {
+return array.map(obj=>{
+  let finalNote = ((obj.firstProject+obj.secondProject)/2*0.4)+(obj.finalExam*0.6);
+  return {
+    name: obj.name,
+    finalNote:finalNote
+  }
+})
+}
 
-
-
+console.log(ArrayFinalNote(students))
 
 
 
@@ -120,7 +133,11 @@ const menu = [
 
 //ES5 =>
 
-
+function avgCalories(array){
+  return array.reduce((acc, food)=>{
+    acc + food.calories    
+  },0)
+}
 
 
 //ES6 =>
@@ -136,48 +153,54 @@ const menu = [
 
   //We are developing our super e-commerce website; each product has some user comments and rating stored in an array called “Reviews”. Each review is an object, so we have the following structure:
 
-const product = {
-  name: "AmazonBasics Apple Certified Lightning to USB Cable",
-  price: 7.99,
-  manufacter: "Amazon",
-  reviews:
-  [
-    {  user: "Pavel Nedved",
-      comments: "It was really usefull, strongly recommended",
-      rate: 4
-    },
-    {  user: "Alvaro Trezeguet",
-      comments: "It lasted 2 days",
-      rate: 1
-    },
-    {  user: "David Recoba",
-      comments: "Awesome",
-      rate: 5
-    },
-    {  user: "Jose Romero",
-      comments: "Good value for money",
-      rate: 4
-    },
-    {  user: "Antonio Cano",
-      comments: "It broked really fast",
-      rate: 2
-    },
-  ]
-}
-
-// Hint: We have to show the product on our website, but we do not want to show all the reviews in a first view, we only need the average rate so that other customers can see in a fast way every product rate.
-
-
-
-
-//ES5 =>
-
-
-
-
-//ES6 =>
-
-
+  const product = {
+    name: "AmazonBasics Apple Certified Lightning to USB Cable",
+    price: 7.99,
+    manufacter: "Amazon",
+    reviews:
+    [
+      {  user: "Pavel Nedved",
+        comments: "It was really usefull, strongly recommended",
+        rate: 4
+      },
+      {  user: "Alvaro Trezeguet",
+        comments: "It lasted 2 days",
+        rate: 1
+      },
+      {  user: "David Recoba",
+        comments: "Awesome",
+        rate: 5
+      },
+      {  user: "Jose Romero",
+        comments: "Good value for money",
+        rate: 4
+      },
+      {  user: "Antonio Cano",
+        comments: "It broked really fast",
+        rate: 2
+      },
+    ]
+  }
+  
+  // Hint: We have to show the product on our website, but we do not want to show all the reviews in a first view, we only need the average rate so that other customers can see in a fast way every product rate.
+  
+  
+  
+  
+  //ES5 =>
+  
+  
+  
+  
+  //ES6 =>
+  
+  let newProduct = obj =>{
+    let sum = obj.reviews.reduce((acc, num)=>
+       acc + num.rate
+    ,0)
+    return sum/obj.reviews.length
+  }
+  newProduct(product)
 
 
 
@@ -189,13 +212,13 @@ const product = {
 // Given the following array, filter the ones who are eligible to go to bars in the USA.
 
 var people = [
-    { name: "Candice", age: 25 },
-    { name: "Tammy", age: 30 },
-    { name: "Allen", age: 20 },
-    { name: "Nettie", age: 21 },
-    { name: "Stuart", age: 17 },
-    { name: "Bill", age: 19 }
-  ];
+  { name: "Candice", age: 25 },
+  { name: "Tammy", age: 30 },
+  { name: "Allen", age: 20 },
+  { name: "Nettie", age: 21 },
+  { name: "Stuart", age: 17 },
+  { name: "Bill", age: 19 }
+];
 
 
 
@@ -206,6 +229,8 @@ var people = [
 
 
 //ES6 =>
+let canDrink = people => people.filter(person=>person.age>20);
+console.log(canDrink(people))
 
 
 
@@ -224,77 +249,77 @@ var people = [
 // Giving the following arrays of objects, let’s filter just the one with a pool.
 
 const places = [
-    {
-     title: "Awesome Suite 20' away from la Rambla",
-     price: 200,
-     type: "Private Room",
-     pool: true,
-     garage: false
-    },
-    {
-     title: "Private apartment",
-     price: 190,
-     type: "Entire Place",
-     pool: true,
-     garage: true
-    },
-    {
-     title: "Apartment with awesome views",
-     price: 400,
-     type: "Entire Place",
-     pool: false,
-     garage: false
-    },
-    {
-     title: "Apartment in la Rambla",
-     price: 150,
-     type: "Private Room",
-     pool: false,
-     garage: true
-    },
-    {
-     title: "Comfortable place in Barcelona´s center",
-     price: 390,
-     type: "Entire place",
-     pool: true,
-     garage: true
-    },
-    {
-     title: "Room near Sagrada Familia",
-     price: 170,
-     type: "Private Room",
-     pool: false,
-     garage: false
-    },
-    {
-     title: "Great house next to Camp Nou",
-     price: 140,
-     type: "Entire place",
-     pool: true,
-     garage: true
-    },
-    {
-     title: "New apartment with 2 beds",
-     price: 2000,
-     type: "Entire place",
-     pool: false,
-     garage: true
-    },
-    {
-     title: "Awesome Suite",
-     price: 230,
-     type: "Private Room",
-     pool: false,
-     garage: false
-    },
-    {
-     title: "Apartment 10' from la Rambla",
-     price: 930,
-     type: "Entire place",
-     pool: true,
-     garage: true
-    }
-   ]
+  {
+   title: "Awesome Suite 20' away from la Rambla",
+   price: 200,
+   type: "Private Room",
+   pool: true,
+   garage: false
+  },
+  {
+   title: "Private apartment",
+   price: 190,
+   type: "Entire Place",
+   pool: true,
+   garage: true
+  },
+  {
+   title: "Apartment with awesome views",
+   price: 400,
+   type: "Entire Place",
+   pool: false,
+   garage: false
+  },
+  {
+   title: "Apartment in la Rambla",
+   price: 150,
+   type: "Private Room",
+   pool: false,
+   garage: true
+  },
+  {
+   title: "Comfortable place in Barcelona´s center",
+   price: 390,
+   type: "Entire place",
+   pool: true,
+   garage: true
+  },
+  {
+   title: "Room near Sagrada Familia",
+   price: 170,
+   type: "Private Room",
+   pool: false,
+   garage: false
+  },
+  {
+   title: "Great house next to Camp Nou",
+   price: 140,
+   type: "Entire place",
+   pool: true,
+   garage: true
+  },
+  {
+   title: "New apartment with 2 beds",
+   price: 2000,
+   type: "Entire place",
+   pool: false,
+   garage: true
+  },
+  {
+   title: "Awesome Suite",
+   price: 230,
+   type: "Private Room",
+   pool: false,
+   garage: false
+  },
+  {
+   title: "Apartment 10' from la Rambla",
+   price: 930,
+   type: "Entire place",
+   pool: true,
+   garage: true
+  }
+ ]
 
 
 
@@ -306,7 +331,8 @@ const places = [
 
 
 //ES6 =>
-
+let filterPools = array => array.filter(house=>house.pool)
+filterPools(places)
 
 
 
@@ -324,11 +350,11 @@ const numbers = [1, 60, 112, 123, 100, 99, 73, 45];
 
 //ES5 =>
 
+let filterNumbers = array => array.filter(num=>num>42 && num%2 ===0)
 
-
+console.log(filterNumbers(numbers))
 
 //ES6 =>
-
 
 
 
